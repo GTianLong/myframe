fe3487ce2fc1cb2a341753e07c62a669795dd9f9
gaotianlong <869553051@qq.com>
2019-01-23 13:50:38 +0800

    初次提交

---
+++ src/main/java/com/example/myframe/MyframeApplication.java
@@ -0,0 +1,14 @@
+package com.example.myframe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyframeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyframeApplication.class, args);
+    }
+
+}
+
